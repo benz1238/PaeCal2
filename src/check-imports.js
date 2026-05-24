dcddf005e7498860e17fe29d21c21b5ea83dbbe7
benz1238 <thanawat.singkil@gmail.com>
2026-05-25 04:59:47 +0700

    Create check-imports.js

---
+++ src/check-imports.js
@@ -0,0 +1,10 @@
+await import("./services/openai.js");
+await import("./services/sheet.js");
+await import("./services/line.js");
+await import("./utils/helpers.js");
+await import("./utils/profile.js");
+await import("./utils/advice.js");
+await import("./handlers/textHandler.js");
+await import("./handlers/imageHandler.js");
+
+console.log("All imports are OK");
